0f9af2cb0e6abab001312ee90191c6d309a54306
buctor41 <buctor41@gmail.com>
2021-07-26 14:06:59 +0800

    lab 8 finish

---
+++ sram/CPU_CDE/mycpu_verify/rtl/myCPU/mycpu_top.v
@@ -0,0 +1,97 @@
+`include "vie_define.h"
+module mycpu_top(
+    input         clk,
+    input         resetn,
+    input  [ 5:0] ext_int,
+    // inst sram interface
+    output        inst_sram_en,
+    output [ 3:0] inst_sram_wen,
+    output [31:0] inst_sram_addr,
+    output [31:0] inst_sram_wdata,
+    input  [31:0] inst_sram_rdata,
+    // data sram interface
+    output        data_sram_en,
+    output [ 3:0] data_sram_wen,
+    output [31:0] data_sram_addr,
+    output [31:0] data_sram_wdata,
+    input  [31:0] data_sram_rdata,
+    // trace debug interface
+    output [31:0] debug_wb_pc,
+    output [ 3:0] debug_wb_rf_wen,
+    output [ 4:0] debug_wb_rf_wnum,
+    output [31:0] debug_wb_rf_wdata
+);
+reg         reset;
+always @(posedge clk) reset <= ~resetn;
+
+wire         ds_allowin;
+wire         es_allowin;
+wire         ms_allowin;
+wire         ws_allowin;
+
+wire [`Vbrbus       -1:0] brbus;
+wire [`Vfsbus       -1:0] fsbus;
+wire [`Vfromifcbus  -1:0] ifc_inst;     //取到的指�???
+wire [`Vtoifcbus    -1:0] inst_ifc;     //
+wire [`Vfromifcbus  -1:0] ifc_data;
+wire [`Vtoifcbus    -1:0] data_ifc;
+wire [`Vissuebus    -1:0] issuebus;
+wire [`Vwsbus       -1:0] wsbus   ;
+wire [`Vrsbus       -1:0] rsbus   ;
+wire [`Vmsbus       -1:0] msbus   ;
+wire [`Vdebugbus    -1:0] debugbus;
+wire [`Vflushbus    -1:0] flushbus;
+wire [`Vrstatus     -1:0] rstatus ;
+wire [`Vmstatus     -1:0] mstatus ;
+wire [`Vwstate      -1:0] wstate  ;
+wire [`Vmstate      -1:0] mstate  ;
+wire [`Vestate      -1:0] estate  ;
+
+assign        ifc_inst = inst_sram_rdata;
+assign    inst_sram_en = inst_ifc[68:68];
+assign   inst_sram_wen = inst_ifc[67:64];
+assign  inst_sram_addr = inst_ifc[63:32];
+assign inst_sram_wdata = inst_ifc[31: 0];
+
+assign        ifc_data = data_sram_rdata;
+assign    data_sram_en = data_ifc[68:68];
+assign   data_sram_wen = data_ifc[67:64];
+assign  data_sram_addr = data_ifc[63:32];
+assign data_sram_wdata = data_ifc[31: 0];
+
+assign debug_wb_pc       = debugbus[72:41];
+assign debug_wb_rf_wen   = debugbus[40:37];
+assign debug_wb_rf_wnum  = debugbus[36:32];
+assign debug_wb_rf_wdata = debugbus[31: 0];
+
+
+
+vie_if_stage  u0_if_stage(
+    .clock(clk)    , .reset(reset)        , .ds_allowin(ds_allowin), .brbus_i(brbus),
+    .fsbus_o(fsbus), .ifc_inst_i(ifc_inst), .inst_ifc_o(inst_ifc)  , .flushbus_i(flushbus)
+);
+
+vie_id_stage  u1_id_stage(
+    .clock(clk)    , .reset(reset)        , .es_allowin(es_allowin), .fsbus_i(fsbus),
+    .brbus_o(brbus), .issuebus_o(issuebus), .ds_allowin(ds_allowin), .wsbus_i(wsbus),
+    .rstatus_i(rstatus), .mstatus_i(mstatus), .flushbus_i(flushbus), .estate_i(estate)
+);
+
+vie_exe_stage u2_ex_stage(
+    .clock(clk)          , .reset(reset)        , .es_allowin(es_allowin), .issuebus_i(issuebus),
+    .rsbus_o(rsbus)      , .data_ifc_o(data_ifc), .ms_allowin(ms_allowin), .rstatus_o(rstatus) ,
+    .flushbus_i(flushbus), .estate_o  (estate)  , .mstate_i(mstate)
+);
+
+vie_mem_stage u3_mem_stage(
+    .clock(clk)          , .reset(reset)        , .ws_allowin(ws_allowin), .rsbus_i(rsbus),
+    .msbus_o(msbus)      , .ifc_data_i(ifc_data), .ms_allowin(ms_allowin), .mstatus_o(mstatus),
+    .flushbus_i(flushbus), .wstate_i(wstate)    , .mstate_o(mstate)
+);
+
+vie_wb_stage  u4_wb_stage(
+    .clock(clk)    , .reset(reset)        , .ws_allowin(ws_allowin), .msbus_i(msbus)      ,
+    .wsbus_o(wsbus), .debugbus_o(debugbus), .ext_int_in(ext_int)   , .flushbus_o(flushbus),
+    .wstate_o(wstate)
+);
+endmodule
---
+++ sram/CPU_CDE/mycpu_verify/rtl/myCPU/vie_if.v
@@ -0,0 +1,109 @@
+module vie_if_stage(
+    clock,
+    reset,
+
+    ds_allowin,
+    brbus_i,
+    fsbus_o,
+
+    ifc_inst_i,
+    inst_ifc_o,
+    flushbus_i
+);
+input                               clock;
+input                               reset;
+
+input                               ds_allowin;
+input  [`Vbrbus               -1:0] brbus_i;
+output [`Vfsbus               -1:0] fsbus_o;
+
+input  [`Vfromifcbus          -1:0] ifc_inst_i;
+output [`Vtoifcbus            -1:0] inst_ifc_o;
+
+input  [`Vflushbus            -1:0] flushbus_i;
+//wire        br_taken  = brbus_i[64:64];
+//wire [31:0] br_base   = brbus_i[63:32];
+//wire [31:0] br_offset = brbus_i[31: 0];       
+wire        br_taken  = brbus_i[32:32];
+wire [31:0] br_target = brbus_i[31: 0];
+
+
+wire        flush_taken  = flushbus_i[32:32];
+wire [31:0] flush_target = flushbus_i[31: 0];
+
+reg  [31:0] fs_pc_r;
+wire [31:0] fs_inst;
+wire [ 5:0] fs_exc    ;
+wire        fs_to_ds_valid;
+
+assign fsbus_o[70:70] = fs_to_ds_valid;
+assign fsbus_o[69:64] = fs_exc; 
+assign fsbus_o[63:32] = fs_pc_r;
+assign fsbus_o[31: 0] = fs_inst;
+
+assign fs_inst        = ifc_inst_i;
+
+wire        inst_sram_en;
+wire [3 :0] inst_sram_wen;
+wire [31:0] inst_sram_addr;
+wire [31:0] inst_sram_wdata;
+
+assign inst_ifc_o[68:68] = inst_sram_en;
+assign inst_ifc_o[67:64] = inst_sram_wen;
+assign inst_ifc_o[63:32] = inst_sram_addr;
+assign inst_ifc_o[31: 0] = inst_sram_wdata;
+
+/*
+ *  Internal Signals and registers
+ */
+reg  fs_valid_r;
+wire fs_cango;
+wire fs_allowin;
+wire to_fs_valid;
+
+wire exc_adel;
+wire [31:0] seq_pc ;
+wire [31:0] next_pc;
+
+
+
+
+
+//pre-if
+assign to_fs_valid  = ~reset;
+assign seq_pc       = fs_pc_r + 3'h4;
+assign next_pc      = flush_taken ? flush_target :
+                      br_taken    ? br_target    : seq_pc;
+//assign pc_adder_a   = br_taken ? br_base   : fs_pc_r;
+//assign pc_adder_b   = br_taken ? br_offset : 32'h4  ;
+//assign pc_adder_res = pc_adder_a + pc_adder_b;
+//assign next_pc      = pc_adder_res;
+//if
+assign fs_cango       = 1'b1;
+assign fs_allowin     = !fs_valid_r || fs_cango && ds_allowin;
+assign fs_to_ds_valid = fs_valid_r && fs_cango;
+
+always @(posedge clock) begin
+    if(reset) begin
+        fs_valid_r <= 1'b0;
+    end else if(fs_allowin) begin
+        fs_valid_r <= to_fs_valid;
+    end
+
+    if(reset) begin
+        fs_pc_r <= 32'hbfbffffc;
+    end else if(to_fs_valid && fs_allowin) begin
+        fs_pc_r <= next_pc;
+    end
+end
+
+assign exc_adel         = to_fs_valid && fs_allowin && (next_pc[1:0]!=2'b00);
+assign fs_exc           = {exc_adel,5'b00};
+
+assign inst_sram_en     = to_fs_valid && fs_allowin;
+assign inst_sram_wen   = 4'h0;
+assign inst_sram_addr  = next_pc;
+assign inst_sram_wdata = 32'b0;
+
+
+endmodule
